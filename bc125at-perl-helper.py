5a3b44f741be674654822a4f6e204b92f1881f92
itsmaxymoo <maxoman10@gmail.com>
2019-08-22 12:19:09 -0400

    CSV to TXT

--- bc125at-perl-helper.py
+++ bc125at-perl-helper.py
@@ -2,8 +2,10 @@
 
 # Imports
 import sys
+import io
 import json
 import re
+import csv
 
 # Constants
 NAME = "BC125AT-Perl Helper"
@@ -42,7 +44,7 @@ else:
 	operation = OPERATION_TO_CSV
 
 # Begin conversion
-print("Converting " + inFileName + " to " + "CSV" if operation == OPERATION_TO_CSV else "Text")
+print("Converting " + inFileName + " to " + ("CSV" if operation == OPERATION_TO_CSV else "Text"))
 inData = inFile.read()
 inFile.close()
 outData = None
@@ -72,7 +74,50 @@ if operation == OPERATION_TO_CSV:
 		exit(1)
 else:
 	# Convert CSV to TXT
-	print("TODO")
+	inData = inData.replace("Name,Frequency,Modulation,CTCSS Tone,Delay,Locked Out,Priority\n", "")
+	# Read CSV
+	inData = csv.reader(io.StringIO(inData))
+	inData = list(inData)
+
+	if len(inData) != 500:
+		print("ERROR: Total channels does not equal 500! (" + str(len(inData)) + ")")
+		exit(1)
+
+	# Setup output file
+	outData = "[\n"
+
+	# Generate output data
+	ind = 1
+	for c in inData:
+		outData += "{\n"
+		outData += "cmd => 'CIN',\n"
+		outData += "index => '" + str(ind) + "',\n"
+		if len(c[0]) > 16:
+			print("ERROR: \"" + c[0] + "\" is longer than 16 characters!")
+			exit(1)
+		outData += "name => '" + c[0] + "',\n"
+		outData += "frq => '" + c[1] + "',\n"
+		if c[2] not in ["FM", "NFM", "AM", "AUTO"]:
+			print("ERROR: Unknown modulation: \"" + c[2] + "\"")
+			exit(1)
+		outData += "mod => '" + c[2] + "',\n"
+		outData += "ctcss_dcs => '" + c[3] + "',\n"
+		if int(c[4]) < 0:
+			print("ERROR: Delay must be >=0!")
+			exit(1)
+		outData += "dly => '" + c[4] + "',\n"
+		if c[5] != "0" and c[5] != "1":
+			print("ERROR: Lockout must be either 0 or 1!")
+			exit(1)
+		outData += "lout => '" + c[5] + "',\n"
+		if c[6] != "0" and c[6] != "1":
+			print("ERROR: Priority must be either 0 or 1!")
+			exit(1)
+		outData += "pri => '" + c[5] + "',\n"
+		outData += "},\n"
+
+		ind += 1
+	outData += "]\n"
 	
 outFileName = sys.argv[2]
 try:
